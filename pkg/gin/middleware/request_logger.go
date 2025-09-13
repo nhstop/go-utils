5df8346512cea68725587b14bdaa8c7e963603a4
ajayratha4 <ajayratha4@gmail.com>
2025-09-13 20:23:36 +0530

    added log

--- pkg/gin/middleware/request_logger.go
+++ pkg/gin/middleware/request_logger.go
@@ -1,10 +1,18 @@
-package middleware
+package middlewares
 
 import (
 	"time"
 
-	"github.com/busnosh/go-utils/pkg/log"
+	"github.com/busnosh/go-utils/pkg/logger"
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue   = "\033[34m"
 )
 
 func RequestLogger() gin.HandlerFunc {
@@ -14,13 +22,32 @@ func RequestLogger() gin.HandlerFunc {
 		// Process request
 		c.Next()
 
-		// After request
-		duration := time.Since(start)
+		latency := time.Since(start)
 		status := c.Writer.Status()
 		method := c.Request.Method
 		path := c.Request.URL.Path
 
-		// Use your custom logger
-		log.Info("%s %s -> %d (%v)", method, path, status, duration)
+		// Choose status color
+		statusColor := ColorGreen
+		switch {
+		case status >= 500:
+			statusColor = ColorRed
+		case status >= 400:
+			statusColor = ColorYellow
+		case status >= 300:
+			statusColor = ColorBlue
+		}
+
+		// Capture errors if any
+		errorMessage := ""
+		if len(c.Errors) > 0 {
+			errorMessage = c.Errors.String()
+		}
+
+		// Log the request using your custom logger
+		logger.Info("%s %s | %s%d%s | %v", method, path, statusColor, status, ColorReset, latency)
+		if errorMessage != "" {
+			logger.Error("Error: %s%s%s", statusColor, errorMessage, ColorReset)
+		}
 	}
 }
